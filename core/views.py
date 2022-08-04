6ff3e345b4159504a9d3eb0958eb99b0adc4e9a1
talha iqbal <taha.iqbal@arbisoft.com>
2022-08-04 11:37:23 +0500

    talha's changes

--- core/views.py
+++ core/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def question_view(request):
-    template_name = "template/base.html"
+    template_name = "base.html"
     return render(request, template_name)
 
 
 def tags_view(request):
-    template_name = "template/base.html"
+    template_name = "base.html"
     return render(request, template_name)
 
 
 def answer_view(request):
-    template_name = "template/base.html"
+    template_name = "base.html"
     return render(request, template_name)
